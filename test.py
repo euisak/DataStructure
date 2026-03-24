aa596abcfde148454f78acd0f0b9fd3599c6dfa0
euisak <166041154+euisak@users.noreply.github.com>
2026-03-24 12:55:32 +0900

    test.py upload

---
+++ test.py
@@ -0,0 +1,23 @@
+# 코드 1.1: Bag의 주요 연산을 일반함수로 구현 예.
+def contains(bag, e):
+    return e in bag
+
+def insert(bag, e):
+    bag.append(e)
+
+def remove(bag, e):
+    bag.remove(e)
+
+def count(bag):
+    return len(bag)
+
+# 코드 1.2: Bag을 활용한 테스트 프로그램
+myBag = []
+insert(myBag, 'apple')
+print('내 가방속의 물건:', myBag)
+
+insert(myBag, 'banana')
+print('내 가방속의 물건:', myBag)
+
+remove(myBag, myBag[0])
+print('내 가방속의 물건:', myBag)
